581b85d28e110c6339e27784dc34d71f2a5a4991
danil555555 <maslovdanil45228@gmail.com>
2024-12-01 02:53:25 +0300

    Create controllers

---
+++ GameStore/Controllers/CartItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Controllers
+{
+    public class CartItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
